9f600fb6a72204803746b0f836ac635529a92df0
Binu David <BinuDavid@outlook.com>
2021-03-18 22:18:25 -0400

    Structure, Style, Validation updates
    Sematic HTML5 updates, CSS styling, along with basic form validation using pattern and JS.

--- server.js
+++ server.js
@@ -2,18 +2,39 @@
 projectData = {};
 
 // Require Express to run server and routes
-
+const express = require("express");
 // Start up an instance of app
+const app = express();
 
 /* Middleware*/
+const bodyParser = require("body-parser");
 //Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
+const cors = require("cors");
 
 // Initialize the main project folder
-app.use(express.static('website'));
-
+app.use(express.static("website"));
 
 // Setup Server
+const port = 3000;
+
+const server = app.listen(port, () => {
+  console.log(`The server is running on port ${port}`);
+});
+
+// GET route
+app.get("/", function (req, res) {
+  res.send(projectData);
+});
+
+// POST route
+app.post("/", function (req, res) {
+  const data = req.body;
+
+  projectData.temp = data.temp;
+  projectData.date = data.date;
+  projectData.user = data.user;
+});
--- website/app.js
+++ website/app.js
@@ -1,5 +1,23 @@
 /* Global Variables */
+const base_url = "api.openweathermap.org/data/2.5/weather?zip=";
+const api_key = "&appid=f9073135a549c17ba1e1db22044aeb25";
+const generate = document.querySelector("#generate");
 
 // Create a new date instance dynamically with JS
 let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+let newDate = d.getMonth() + "." + d.getDate() + "." + d.getFullYear();
+
+// Generate- Event Listener
+generate.addEventListener("click", () => {
+  const zip = document.querySelector("#zip");
+  // Zip-Code Validation
+  zip.oninvalid = function (event) {
+    event.target.setCustomValidity(
+      "Please enter a valid 5 digit numeric zip-code"
+    );
+  };
+
+  const zip_code = zip.value;
+
+  console.log(zip_code);
+});
